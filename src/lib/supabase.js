1daebd7e32d41a3bb93dc87cb5802f735ce3c1e9
seguimientodegestionmgp-hue <seguimientodegestionmgp@gmail.com>
2026-06-03 09:46:34 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://qhkzmiceieeiwxxcxwua.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoa3ptaWNlaWVlaXd4eGN4d3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NjU1MjUsImV4cCI6MjA5NjA0MTUyNX0.Z3rLYpHJ8R8EZYRpuFGQVd3kxKR_5u4CWf-3LeYcb0E'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
